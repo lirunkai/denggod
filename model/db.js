3caad8ef8982be7ef370765f31d74546c9e38658
lirunkai <15582889147@163.com>
2016-12-15 13:06:15 +0800

    cons

--- model/db.js
+++ model/db.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose')
 mongoose.Promise = global.Promise;
-var db = mongoose.connect('mongodb://localhost:27017/okcoin');
-
+//var db = mongoose.connect('mongodb://localhost:27017/okcoin');
+var db = mongoose.connect('mongodb://root:Kongxi521@dds-2ze41b282c55fcc41.mongodb.rds.aliyuncs.com:3717,dds-2ze41b282c55fcc42.mongodb.rds.aliyuncs.com:3717/admin?replicaSet=mgset-2482227');
 module.exports = db;
