8d82fe4b502477f6fd9fc49dc26b9c950379dea0
lirunkai <15582889147@163.com>
2016-12-19 13:36:57 +0800

    wechat01

--- routes/start.js
+++ routes/start.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 var User = require('../model/user')
+var OAuth = require('wechat-oauth');
+
+var client = new OAuth('wx60aeb0c0c8970d98','cae9z13dccd05d8786abdeeb29b6b152');
+
+// router.get('/', function(req, res){
+//   res.redirect('/callback');
+// })
 
 router.get('/', function(req, res){
-  res.redirect('/callback');
+  var url = client.getAuthorizeURL('http://d.zhongpinhappy.cn/callback','','snsapi_userinfo');
+  res.redirect(url)
 })
 
 module.exports = router;
